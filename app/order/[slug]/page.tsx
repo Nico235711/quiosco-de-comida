65b2a0b778a91355a127f5a23d975ab02ecfc67e
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-08 17:05:13 -0300

    Detectar la Categoría actual y obtener los Productos

--- app/order/[slug]/page.tsx
+++ app/order/[slug]/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+import { prisma } from "@/src/lib/prisma";
 
-export default function OrderPage({ params }: { params: { slug: string } }) {
-  console.log(params.slug);
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: { slug: category }
+    }
+  })
+
+  return products 
+}
+
+export default async function OrderPage({ params }: { params: { slug: string } }) {
+
+  const products = await getProducts(params.slug)
+  console.log(products);
   
+
   return (
     <h1>OrderPage</h1>
   );
