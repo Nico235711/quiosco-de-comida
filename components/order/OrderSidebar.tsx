a7a1b6a0390baf7bb8987c86a715763079922b6c
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-08 15:50:11 -0300

    Data Fetching en Next.js

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,8 +1,14 @@
+import { prisma } from "@/src/lib/prisma"
 
+async function getCatories() {
+  return await prisma.category.findMany()  
+}
 
-export default function OrderSideBar() {
+export default async function OrderSideBar() {
 
-  return (
+  const categories = await getCatories()  
+
+  return (  
     <aside className="bg-white md:h-screen md:w-72">
       OrderSideBar
     </aside>
