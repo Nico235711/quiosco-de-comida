5d89e819ee33e96f7374752e74b9d0b1f5d82201
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-07 21:23:26 -0300

    Creando los contenedores principales

---
+++ app/products/new/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function NewProductPage() {
+
+  return (
+    <div>NewProductPage</div>
+  )
+}
+
+
+export const page = () => {
+  return (
+    <div>page</div>
+  )
+}
---
+++ components/order/OrderSidebar.tsx
@@ -0,0 +1,10 @@
+
+
+export default function OrderSideBar() {
+
+  return (
+    <aside className="bg-white md:h-screen md:w-72">
+      OrderSideBar
+    </aside>
+  )
+}
