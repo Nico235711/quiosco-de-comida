94793396ae46d5be79f8b211b6dec30b30f2415d
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-08 19:03:26 -0300

    Finalizando los Card de Productos

--- app/order/[slug]/page.tsx
+++ app/order/[slug]/page.tsx
@@ -18,6 +18,7 @@ export default async function OrderPage({ params }: { params: { slug: string } }
 
   return (
     <>
+      <h1 className="text-4xl text-center font-black mb-5">Elige y personaliza tu pedido</h1>
       <div className="grid gap-4 grid-cols-1 lg:grid-cols-3">
         {products.map(product => (
           <ProductCard key={product.id} product={product} />
--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -10,17 +10,21 @@ export default function ProductCard({ product }: ProductCardProps) {
 
   return (
     <div className="bg-white border p-5">
-      <div className="relative w-60 h-60">
-        <Image
-          src={`/products/${product.image}.jpg`}
-          alt={`image de ${product.name}`}
-          fill
-        />
-      </div>
+      <Image
+        src={`/products/${product.image}.jpg`}
+        alt={`image de ${product.name}`}
+        width={400}
+        height={400}
+      />
 
       <div>
-        <h3 className="text-xl font-black">{product.name}</h3>
-        <p className="text-lg text-amber-400 font-black mt-5">{formatCurrency(product.price)}</p>
+        <h3 className="text-xl font-black mt-5 h-16">{product.name}</h3>
+        <p className="text-2xl text-amber-500 font-black mt-5">{formatCurrency(product.price)}</p>
+        <input
+          type="button"
+          value="Agregar al Pedido" 
+          className="bg-black text-white py-1 px-4 mt-5 w-full text-xl flex justify-end"
+        />
       </div>
     </div>
   )
