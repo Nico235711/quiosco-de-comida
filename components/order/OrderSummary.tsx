f114075782d0a55330c113ccbe8d97abe0da9ed9
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-09 16:03:47 -0300

    Mostrando los articulos de la orden, Evitar Duplicados, Incrementando la cantidad de un articulo, Drecrementar la cantidad de un artículo, Calculando el Total a Pagar

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,11 +1,15 @@
 "use client" // componente de cliente
 
 import { useStore } from "@/src/store"
+import ProductDetails from "./ProductDetails"
+import { formatCurrency } from "@/src/utils"
+import { useMemo } from "react"
 
 
 export default function OrderSummary() {
   
   const order = useStore((state) => state.order)
+  const total = useMemo(() => order.reduce((total, item) => total + item.subTotal, 0), [order])
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
@@ -13,7 +17,14 @@ export default function OrderSummary() {
       {order.length === 0 ? (
         <p className="text-2xl text-center mt-5">La orden esta vacía</p>
       ) : (
-        <p>hay</p>
+        <div className="mt-5">
+          {order.map(item => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
+          <p className="mt-5 text-2xl">Total a Pagar: {""} 
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
+        </div>
       )}
     </aside>
   )
--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,72 @@
 import { create } from "zustand";
 import { OrderItem } from "./types";
 import { Product } from "@prisma/client";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 interface Store {
   order: OrderItem[]
-  addOrder: (product: Product) => void
+  addToOrder: (product: Product) => void
+  removeFromOrder: (id: OrderItem["id"]) => void
+  increaseQuantity: (id: OrderItem["id"]) => void
+  decreaseQuantity: (id: OrderItem["id"]) => void
 }
 
-export const useStore = create<Store>()(devtools((set) => ({
-  order: [],
-  addOrder: (product) => {
-    // saco las propiedades que no quiero
-    const { categoryId, image, ...data } = product
+export const useStore = create<Store>()(
+  devtools(
+    persist((set, get) => ({
+      order: [],
+      addToOrder: (product) => {
+        // saco las propiedades que no quiero
+        const { categoryId, image, ...data } = product
+        let order: OrderItem[] = []
 
-    set((state) => ({
-      order: [...state.order, {
-        ...data,
-        quantity: 1,
-        subTotal: 1 * product.price
-      }]
-    }))
-  }
-})))
+        if (get().order.find(item => item.id === product.id)) {
+          // localizo el item que debo modificar la cantidad y el subTotal
+          order = get().order.map(item => item.id === product.id ? {
+            ...item,
+            quantity: item.quantity + 1,
+            subTotal: (item.quantity + 1) * item.price
+          } : item)
+        } else {
+          order = [...get().order, {
+            ...data,
+            quantity: 1,
+            subTotal: 1 * product.price
+          }]
+        }
+        set(({
+          order
+        }))
+      },
+      removeFromOrder: (id) => {
+        set((state) => ({
+          order: state.order.filter(item => item.id !== id)
+        })
+        )
+      },
+      increaseQuantity: (id) => {
+        set((state) => ({
+          order: get().order.map(item => item.id === id ? {
+            ...item,
+            quantity: item.quantity + 1,
+            subTotal: (item.quantity + 1) * item.price
+          } : item)
+        }))
+      },
+      decreaseQuantity: (id) => {
+        const order = get().order.map(item => item.id === id ? {
+          ...item,
+          quantity: item.quantity - 1,
+          subTotal: (item.quantity - 1) * item.price
+        } : item)
+        set((state) => ({
+          order 
+        }))
+      }
+    }),
+      {
+        name: "order-storage"
+      }
+    )
+  )
+)
