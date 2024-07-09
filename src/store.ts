039b7188277ed6cfdd1f4071dbdafebede119908
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-09 14:28:12 -0300

    Agregar articulos a la orden

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,25 @@
 import { create } from "zustand";
 import { OrderItem } from "./types";
 import { Product } from "@prisma/client";
+import { devtools } from "zustand/middleware";
 
 interface Store {
   order: OrderItem[]
   addOrder: (product: Product) => void
 }
 
-export const useStore = create<Store>(() => ({
+export const useStore = create<Store>()(devtools((set) => ({
   order: [],
   addOrder: (product) => {
-    console.log(`agregando: ${product}`);
-    
+    // saco las propiedades que no quiero
+    const { categoryId, image, ...data } = product
+
+    set((state) => ({
+      order: [...state.order, {
+        ...data,
+        quantity: 1,
+        subTotal: 1 * product.price
+      }]
+    }))
   }
-}))
+})))
