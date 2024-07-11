96ccee1701f2aae6da962c45fa41755589bd176c
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-11 16:58:45 -0300

    Creando un Paginador

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,8 +2,12 @@ import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts() {
+async function getProducts(currentPage: number, pageSize: number) {
+  const skip = (currentPage - 1) * pageSize
+
   return prisma.product.findMany({
+    take: pageSize, // equivalente a un LIMIT en sql
+    skip,
     include: {
       category: true
     }
@@ -12,9 +16,11 @@ async function getProducts() {
 
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
 
-export default async function ProductsPage() {  
+export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {  
 
-  const products = await getProducts()  
+  const currentPage = +searchParams.page || 1
+  const pageSize = 10
+  const products = await getProducts(currentPage, pageSize)
 
   return (
     <>
