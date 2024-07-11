bfb574caf077426cf0a622598c15dca990cf02d9
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-11 13:46:41 -0300

    Creando un componente re-utilizable para los titulos

--- app/order/[slug]/page.tsx
+++ app/order/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/products/ProductCard";
+import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 async function getProducts(category: string) {
@@ -17,7 +18,7 @@ export default async function OrderPage({ params }: { params: { slug: string } }
 
   return (
     <>
-      <h1 className="text-4xl text-center font-black mb-5">Elige y personaliza tu pedido</h1>
+      <Heading label="Elige y Personaliza tu pedido" />
       <div className="grid gap-4 grid-cols-1 lg:grid-cols-3">
         {products.map(product => (
           <ProductCard key={product.id} product={product} />
---
+++ components/ui/Heading.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Heading({ label }: { label: string }) {
+
+  return (
+    <h1 className="text-4xl text-center font-black mb-5">{label}</h1>
+  )
+}
