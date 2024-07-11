2b0ff248d94628e7d55cdfe83ba89a3aaf14b2af
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-11 15:47:09 -0300

    Marcar Ordenes Completas y Validación

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,5 +1,22 @@
 "use server"
 
-export async function completeOrder() {
-  console.log("🚀 ~ completeOrder ~ completeOrder")    
+import { prisma } from "@/src/lib/prisma";
+
+export async function completeOrder(formData: FormData) {
+  const orderId = formData.get("order_id")!
+  try {
+    await prisma.order.update({
+      where: {
+        id: +orderId
+      },
+      data: {
+        status: true,
+        orderReadyAt: new Date(Date.now())
+      }
+    })
+
+  } catch (error) {
+    console.log(error);
+    
+  }
 }
