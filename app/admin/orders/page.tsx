46e3a5792ff92cf3c994ab6dcfde3e03e67fa9f4
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-11 14:34:10 -0300

    Obtener las Ordenes Pendientes

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,11 +1,31 @@
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true
+        }
+      }
+    }
+  })
+  return orders
+}
 
-export default function OrdersPage() {
+export default async function OrdersPage() {
+
+  const orders = await getPendingOrders()  
+console.log(orders);
 
   return (
     <>
       <Heading label="Administrar Ordenes" />
+      {/* {orders.map(order => order)} */}
     </>
   )
 }
