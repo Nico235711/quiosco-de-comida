f6f597bf9498fe67d7dea3d04220461aaa1fed9a
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-12 16:15:10 -0300

    Mostrando los resultados de búsqueda

--- app/admin/products/search/page.tsx
+++ app/admin/products/search/page.tsx
@@ -1,14 +1,38 @@
+import ProductSearchForm from "@/components/products/ProductSearchForm";
+import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
+async function searchProducts(searchTerm: string) {
+  return await prisma.product.findMany({
+    where: {
+      name: {
+        contains: searchTerm,
+        mode: "insensitive"
+      }
+    },
+    include: {
+      category: true
+    }
+  })
+}
 
 export default async function SearchPage({ searchParams }: { searchParams: { search: string } }) {
 
-  console.log(searchParams);
-  
+  const products = await searchProducts(searchParams.search)  
 
   return (
     <>
-      <Heading label="Resultados de Búsqueda" />
+      <Heading label={`Resultados de Búsqueda de la Categoría: ${searchParams.search}`} />
+      <div className="flex flex-col lg:flex-row lg:justify-end">
+        <ProductSearchForm />
+      </div>
+      {products.length === 0 ? (
+        <p className="text-2xl mt-5">No hay productos en esta categoría</p>
+      ) : (
+        <ProductsTable products={products} />
+
+      )}
     </>
   )
 }
