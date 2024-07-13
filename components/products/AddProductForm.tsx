f53130f5e7b0bdb2923003564197abc403993f9a
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-07-13 19:41:36 -0300

    Validando la creación de Productos

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -2,11 +2,23 @@
 
 import { ReactNode } from "react";
 import ProductForm from "./ProductForm";
+import { ProductSchema } from "@/src/schema";
+import { toast } from "react-toastify";
 
 export default async function AddProductForm({children}: {children: ReactNode}) {
 
   const handleSubmit = async (formData: FormData) => {
-    console.log("🚀 ~ handleSubmit ~ handleSubmit")
+    const data = {
+      name: formData.get("name"),
+      price: formData.get("price"),
+      categoryId: formData.get("categoryId")
+    }
+
+    const result = ProductSchema.safeParse(data)
+    if (!result.success) {
+      result.error.issues.forEach(issue => toast.error(issue.message))
+      return
+    }
   }
 
   return (
